878644f169ea383b16028ce2dce93179eeb78d29
Brandon Corrigall <corrb812@gmail.com>
2025-06-11 14:12:08 -0400

    created a guest list using the provided API. Added functionality for selcting a guest via click. When a guest is clicked, more details about the selected guest now populates the window. Upon clicking the back button you are brought back to the guest list and able to select another guest

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,56 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
-  return <></>;
+  const [guestList, setGuestList] = useState([]);
+  const [guestDetails, setGuestDetails] = useState(null);
+  useEffect(() => {
+    const getGuestList = async () => {
+      const response = await fetch(
+        "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2505-ftb-et-web-ft/guests"
+      );
+      const respJsonObj = await response.json();
+      const allGuests = respJsonObj.data;
+      setGuestList(allGuests);
+    };
+    getGuestList();
+  }, []);
+
+  return (
+    <>
+      {guestDetails ? (
+        <>
+          <h2>Guest Details</h2>
+          <ul className="guest-details">
+            <li>Name: {guestDetails.name}</li>
+            <li>Email: {guestDetails.email}</li>
+            <li>Phone #: {guestDetails.phone}</li>
+            <li>Bio: {guestDetails.bio}</li>
+            <li>Job: {guestDetails.job}</li>
+            <button onClick={() => setGuestDetails(null)}>Back</button>
+          </ul>
+        </>
+      ) : (
+        <>
+          <h1>Guest List</h1>
+          <section>
+            <h2>Name</h2>
+            <h2>Email</h2>
+            <h2>Phone</h2>
+          </section>
+          <ul className="guest-list">
+            {guestList.map((individualGuest) => (
+              <li
+                key={individualGuest.id}
+                onClick={() => setGuestDetails(individualGuest)}
+              >
+                <div>{individualGuest.name}</div>
+                <div>{individualGuest.email}</div>
+                <div>{individualGuest.phone}</div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
 }
